46044ba4dd7ff9eb61f0924fc460303fc7afb1c2
sergey-frolov-pets <pet-projects@sergey-frolov.ru>
2020-06-29 20:02:06 +0300

    After lesson 5

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -2,7 +2,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
 
-class User(db.Model):
+from app import login
+from flask_login import UserMixin
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 Примеры с сайта https://dash.plotly.com/
 Результаты работы примеров после Run смотреть http://127.0.0.1:8050/
 Deployment https://dash.plotly.com/deployment
+
+Уроки Flask по ссылке https://habr.com/ru/post/346306/
