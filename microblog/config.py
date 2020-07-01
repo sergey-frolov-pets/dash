65b31bb596d312bd67fe7077024e34b9280ba493
sergey-frolov-pets <pet-projects@sergey-frolov.ru>
2020-07-01 18:54:24 +0300

    After lesson 9

--- microblog/config.py
+++ microblog/config.py
@@ -15,3 +15,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['pet-projects@sergey-frolov.ru']
+
+    POSTS_PER_PAGE = 3
