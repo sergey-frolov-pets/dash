aba8b9d18f70c94a383bfe3236a9eb880b3c22e2
sergey-frolov-pets <pet-projects@sergey-frolov.ru>
2020-07-01 19:16:44 +0300

    lesson 10 before "Токены сброса пароля"

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,12 +1,15 @@
-
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+mail = Mail(app)
+
 login = LoginManager(app)
 login.login_view = 'login'
 
