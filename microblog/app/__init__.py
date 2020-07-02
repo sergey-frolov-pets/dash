60b1c9bbb1e9b3280698e2ab9edc05bc8c2c54d1
sergey-frolov-pets <pet-projects@sergey-frolov.ru>
2020-07-02 15:49:41 +0300

    after lesson 11

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,9 +15,11 @@ mail = Mail(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу." # <-- я добавил эту строку
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
